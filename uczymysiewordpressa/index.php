0f6b6227d3898c722c9a5d2ff72a6d94be9c1346
aras0 <sara.czelus@gmail.com>
2018-07-17 23:51:50 +0200

    formatter

--- uczymysiewordpressa/index.php
+++ uczymysiewordpressa/index.php
@@ -1,71 +1,65 @@
 <!doctype html>
 <html>
     <head>
-        <meta charset="UTF-8">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
-		integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" 
-		crossorigin="anonymous">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
-		integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <title>uczymysiewardpressa</title>
+			<meta charset="UTF-8">
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"  crossorigin="anonymous">
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"	integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+			<title>uczymysiewardpressa</title>
     </head>
     <body>
-	
-<div class="conteiner">
-	
-	<nav class="navbar navbar-dark bg-primary">
-	 <button class="btn"><i class="fa fa-home"></i></button>
-	   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="nav nav-pills nav-justified nav-fill">
-		  <li class="nav-item active">
-			<a class="nav-link" href="#">Start <span class="sr-only">(current)</span></a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="#first">O Firmie</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="#secend">Oferta</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="#third">Kontakt</a>
-		  </li>
-		  <!--li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			  Dropdown
-			</a>
-			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			  <a class="dropdown-item" href="#">Action</a>
-			  <a class="dropdown-item" href="#">Another action</a>
-			  <div class="dropdown-divider"></div>
-			  <a class="dropdown-item" href="#">Something else here</a>
+			<div class="conteiner">
+				<nav class="navbar navbar-dark bg-primary">
+					<button class="btn"><i class="fa fa-home"></i></button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="nav nav-pills nav-justified nav-fill">
+						<li class="nav-item active">
+						<a class="nav-link" href="#">Start <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#first">O Firmie</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#secend">Oferta</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#third">Kontakt</a>
+						</li>
+						<!--li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Dropdown
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Action</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</div>
+						</li-->
+						<!--li class="nav-item">
+						<a class="nav-link disabled" href="#">Disabled</a>
+						</li-->
+					</ul>
+					<!--form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form-->
+					</div>
+				</nav>
 			</div>
-		  </li-->
-		  <!--li class="nav-item">
-			<a class="nav-link disabled" href="#">Disabled</a>
-		  </li-->
-		</ul>
-		<!--form class="form-inline my-2 my-lg-0">
-		  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		</form-->
-	  </div>
-	</nav>
-	</div>
-        <p>Na Maltę wybrałem się w Kwietniu 2018r. i chciałbym podzielić się z wami moimi przemyśleniami na temat wyprawy na tą piękną wyspę. Planując podróż na Maltę zwłaszcza na własną rękę, warto odpowiednio zaplanować sobie miejsce noclegu.  Zdecydowanie polecam okolice Valetty gdzie kilka miejscowości tak naprawdę tworzy jedno duże miasto i właściwie nie widać przejścia z jednej miejscowości do drugiej. Bardziej przypomina to dzielnice w Polskim mieście. Stolicą Malty jest Valletta miejsce najlepsze na chodzenie wąskimi, zabytkowymi i klimatycznymi uliczkami. Jeśli lubicie chodzić promenadą wzdłuż wybrzeża najlepszym miejscem będzie  St. Julian's.  Jeśli chcecie mieć blisko do najbardziej imprezowej dzielnicy z dużą ilością barów i dyskotek zdecydowanie polecam Paceville. Natomiast typowym portem skąd odpływają wszystkie wycieczki statkami, i jest piękny widok na Vallette ( która jest pięknie oświetlona nocą ) jest Sliema.   
-	Malta jest bardzo małą wyspą i bez problemów można się w ciągu 1,5 godziny przedostać z jednego końca wyspy na drugi. Podróżować można komunikacją autobusową która działa dość sprawnie. Pamiętać tylko trzeba że na Malcie wszystkie przystanki autobusowe są na żądanie i jeśli chcemy żeby autobus się zatrzymał i nas zabrał trzeba pokiwać kierowcy z przystanku. Jeśli jedziemy autobusem trzeba też wcześniej zasygnalizować że chcemy wysiąść. Autobus może się nie zatrzymać jeśli jest już maksymalnie zapełniony ludźmi i nikt nie wysiada. Polecam korzystać z Google Maps które dokładnie pokazuje na jaki przystanek mamy iść, w jaki autobus wsiąść i gdzie się przesiąść żeby dojechać w docelowe miejsce. Polecam kupić tygodniowy bilet na całą komunikację miejską na Malcie i pobliskiej wyspie Gozo, który kosztuje 21 euro. Jeśli decydujecie się na jazdę samochodem pamiętajcie że na Malcie jest ruch Lewostronny, uliczki bardzo wąskie a kierowcy jeżdżą nieostrożnie. Dodam jeszcze, że w nocy jest bardzo ciężko dojechać na lotnisko i najprawdopodobniej będzie trzeba pojechać taksówką, jeśli macie bardzo wczesny wylot. 
-	 Jeśli liczycie na to, że polecicie na Maltę i będziecie się wylegiwać na piaszczystej plaży możecie się trochę rozczarować. Zdecydowanie większa część lini brzegowej Malty to skały lub klify. Na Malcie jest tylko kilka piaszczystych plaż i są położone w dość dużej odległości od typowo turystycznych miejscowości. W dzielnicy Pcevile ( imprezowa część Malty ) znajduje się bardzo mała plaża szeroka na jakieś 15 metrów i długa może na jakieś 100 - 150 metrów. Osobiście byłem na trzech plażach Golden Bay, Ghajn Tuffieha, Gnejna Bay, które znajdują się bardzo blisko siebie, łącznie mają kilkaset metrów plaży i znajdują się w bardzo malowniczych miejscach. </p>
-	<p>Jeśli macie już dosyć leżenia na pisku, polecam się przejść na pieszo pomiędzy tymi trzema plażami. Wymaga to lekkiej wspinaczki na klify otaczające właśnie te plaże, ale widok który nas tam czeka jest tego warty. </p>
-	<img src="IMAG1677_wm.jpg" width="840" height="480">
-	<p id="first">  Malta posiada wiele cudownych miejsc bardzo często opisywanych w przewodnikach jako atrakcja wyspy. Jednak miejscem, które zrobiło na mnie naprawdę duże wrażenie ( a nie jest zbyt popularne i nie jest wymieniane jako główna atrakcja Malty ) jest St. Peters Pool. Dotrzeć można tam jadąc do miejscowości Marsaxlokk. Jest to wioska rybacka słynąca z kolorowych łódek rybackich z wymalowanymi oczami na ich burtach. Jednak żeby dojść do St. Peter’s Pool trzeba opuścić miasteczko i przejść na drugą część półwyspu znajdującego się przy tej miejscowości. St. Peter’s Pool jest małą zatoczką wydrążoną  w skale gdzie fale w majestatyczny sposób rozbijają się o skały. Będąc w okresie w którym tam byłem, spotkałem w tym miejscu bardzo małą ilość turystów. Niewielu ludzi albo wie albo decyduje się iść w to miejsce, co sprawia że w bardzo dużym spokoju można eksplorować tę zatokę. </p>
-	<img src="IMAG1684_wm.jpg" width="840" height="480">
-	<p id="secend">Będąc na Malcie warto też odwiedzić poboczne wyspy Gozo i Comino. Ja osobiście wybrałem się tam wraz z wycieczką kupioną w porcie, która gwarantowała zwiedzanie Wyspy Gozo i wolny czas na Błękitnej Lagunie na wyspie Comino. Jednak teraz po tym jak skorzystałem z tej opcji nie polecam jej. Wyspa Gozo jest na tyle ciekawa że warto zwiedzać ją przez cały dzień. Płynąć wraz z wycieczką statkiem, po dopłynięciu na wyspę jesteśmy wiezieni z jednego miejsca na drugie podczas trzy godzinnej objazdówki. W ten sposób widzimy największe atrakcje Gozo jednak są tam miejsca, w których lepiej spędzić więcej czas. Do niedawna najbardziej rozpoznawalnym miejscem na tej wyspie było Azure Window, jednak w marcu 2017r. roku ten piękny łuk skalny zawalił się do morza. Z Malty na Gozo można przepłynąć na własną rękę promem z Cirkewwa Ferry Terminal. 
-	Podobnie jest z odwiedzeniem Blue Lagune, na wyspie Comino. Podczas wycieczki całodniowej kiedy zwiedzamy obie wyspy, na pobyt na Comino mamy tylko półtora godziny. Blue Lagune jest cudownym miejscem w którym zdecydowanie warto spędzić więcej czasu.  Codziennie przypływa tam bardzo dużo turystów, od rana zajmują najlepsze miejsca wypoczynkowe. Warto wybrać się w to miejsce wcześniej, a płyną tam wodne taksówki którymi można dotrzeć rankiem i wrócić w momencie kiedy uznamy że mamy na to ochotę.</p>
-	<img src="IMAG1691_wm.jpg" width="840" height="480">
-	<p id="third">Wbrew ogólnej opinii uważam że Malta nie jest Tanim miejscem do wypoczynku. Można tam tanio dolecieć i znaleźć tani nocleg jednak ceny wyżywienia są dość wysokie. Jeśli chcemy zjeść w restauracji gdzieś w pobliżu atrakcji turystycznej to musimy się liczyć z tym, że cena dania obiadowego będzie wynosiła od 17 euro w górę. Do tego dochodzi jeszcze koszt jakiegoś picia i cena wzrasta do ponad 20 euro dla jednej osoby. Jeśli chcecie trochę zaoszczędzić warto poszukać restauracji z dala od miejsc uczęszczanych przez turystów. Mi udało się znaleźć bardzo miły lokal uczęszczany przez mieszkańców wyspy, gdzie serwowane były potrawy typowo Maltańskie a ceny były na poziomie 8 - 12 euro. Więc warto przejść się po różnych dzielnicach i sprawdzić co oferują lokalne restauracje.  
-	Na Malcie jest jeszcze bardzo dużo atrakcji które zdecydowanie trzeba odwiedzić i zobaczyć. Warto się tam wybrać przynajmniej na cały tydzień żeby docenić tą wyspę. Mam nadzieję ze te kilka wskazówek które tutaj zaprezentowałem pozwolą wam lepiej zaplanować waszą wyprawę.<p>
-
+			<p>Na Maltę wybrałem się w Kwietniu 2018r. i chciałbym podzielić się z wami moimi przemyśleniami na temat wyprawy na tą piękną wyspę. Planując podróż na Maltę zwłaszcza na własną rękę, warto odpowiednio zaplanować sobie miejsce noclegu.  Zdecydowanie polecam okolice Valetty gdzie kilka miejscowości tak naprawdę tworzy jedno duże miasto i właściwie nie widać przejścia z jednej miejscowości do drugiej. Bardziej przypomina to dzielnice w Polskim mieście. Stolicą Malty jest Valletta miejsce najlepsze na chodzenie wąskimi, zabytkowymi i klimatycznymi uliczkami. Jeśli lubicie chodzić promenadą wzdłuż wybrzeża najlepszym miejscem będzie  St. Julian's.  Jeśli chcecie mieć blisko do najbardziej imprezowej dzielnicy z dużą ilością barów i dyskotek zdecydowanie polecam Paceville. Natomiast typowym portem skąd odpływają wszystkie wycieczki statkami, i jest piękny widok na Vallette ( która jest pięknie oświetlona nocą ) jest Sliema.   
+			Malta jest bardzo małą wyspą i bez problemów można się w ciągu 1,5 godziny przedostać z jednego końca wyspy na drugi. Podróżować można komunikacją autobusową która działa dość sprawnie. Pamiętać tylko trzeba że na Malcie wszystkie przystanki autobusowe są na żądanie i jeśli chcemy żeby autobus się zatrzymał i nas zabrał trzeba pokiwać kierowcy z przystanku. Jeśli jedziemy autobusem trzeba też wcześniej zasygnalizować że chcemy wysiąść. Autobus może się nie zatrzymać jeśli jest już maksymalnie zapełniony ludźmi i nikt nie wysiada. Polecam korzystać z Google Maps które dokładnie pokazuje na jaki przystanek mamy iść, w jaki autobus wsiąść i gdzie się przesiąść żeby dojechać w docelowe miejsce. Polecam kupić tygodniowy bilet na całą komunikację miejską na Malcie i pobliskiej wyspie Gozo, który kosztuje 21 euro. Jeśli decydujecie się na jazdę samochodem pamiętajcie że na Malcie jest ruch Lewostronny, uliczki bardzo wąskie a kierowcy jeżdżą nieostrożnie. Dodam jeszcze, że w nocy jest bardzo ciężko dojechać na lotnisko i najprawdopodobniej będzie trzeba pojechać taksówką, jeśli macie bardzo wczesny wylot. 
+			Jeśli liczycie na to, że polecicie na Maltę i będziecie się wylegiwać na piaszczystej plaży możecie się trochę rozczarować. Zdecydowanie większa część lini brzegowej Malty to skały lub klify. Na Malcie jest tylko kilka piaszczystych plaż i są położone w dość dużej odległości od typowo turystycznych miejscowości. W dzielnicy Pcevile ( imprezowa część Malty ) znajduje się bardzo mała plaża szeroka na jakieś 15 metrów i długa może na jakieś 100 - 150 metrów. Osobiście byłem na trzech plażach Golden Bay, Ghajn Tuffieha, Gnejna Bay, które znajdują się bardzo blisko siebie, łącznie mają kilkaset metrów plaży i znajdują się w bardzo malowniczych miejscach. </p>
+			<p>Jeśli macie już dosyć leżenia na pisku, polecam się przejść na pieszo pomiędzy tymi trzema plażami. Wymaga to lekkiej wspinaczki na klify otaczające właśnie te plaże, ale widok który nas tam czeka jest tego warty. </p>
+			<img src="IMAG1677_wm.jpg" width="840" height="480">
+			<p id="first">  Malta posiada wiele cudownych miejsc bardzo często opisywanych w przewodnikach jako atrakcja wyspy. Jednak miejscem, które zrobiło na mnie naprawdę duże wrażenie ( a nie jest zbyt popularne i nie jest wymieniane jako główna atrakcja Malty ) jest St. Peters Pool. Dotrzeć można tam jadąc do miejscowości Marsaxlokk. Jest to wioska rybacka słynąca z kolorowych łódek rybackich z wymalowanymi oczami na ich burtach. Jednak żeby dojść do St. Peter’s Pool trzeba opuścić miasteczko i przejść na drugą część półwyspu znajdującego się przy tej miejscowości. St. Peter’s Pool jest małą zatoczką wydrążoną  w skale gdzie fale w majestatyczny sposób rozbijają się o skały. Będąc w okresie w którym tam byłem, spotkałem w tym miejscu bardzo małą ilość turystów. Niewielu ludzi albo wie albo decyduje się iść w to miejsce, co sprawia że w bardzo dużym spokoju można eksplorować tę zatokę. </p>
+			<img src="IMAG1684_wm.jpg" width="840" height="480">
+			<p id="secend">Będąc na Malcie warto też odwiedzić poboczne wyspy Gozo i Comino. Ja osobiście wybrałem się tam wraz z wycieczką kupioną w porcie, która gwarantowała zwiedzanie Wyspy Gozo i wolny czas na Błękitnej Lagunie na wyspie Comino. Jednak teraz po tym jak skorzystałem z tej opcji nie polecam jej. Wyspa Gozo jest na tyle ciekawa że warto zwiedzać ją przez cały dzień. Płynąć wraz z wycieczką statkiem, po dopłynięciu na wyspę jesteśmy wiezieni z jednego miejsca na drugie podczas trzy godzinnej objazdówki. W ten sposób widzimy największe atrakcje Gozo jednak są tam miejsca, w których lepiej spędzić więcej czas. Do niedawna najbardziej rozpoznawalnym miejscem na tej wyspie było Azure Window, jednak w marcu 2017r. roku ten piękny łuk skalny zawalił się do morza. Z Malty na Gozo można przepłynąć na własną rękę promem z Cirkewwa Ferry Terminal. 
+			Podobnie jest z odwiedzeniem Blue Lagune, na wyspie Comino. Podczas wycieczki całodniowej kiedy zwiedzamy obie wyspy, na pobyt na Comino mamy tylko półtora godziny. Blue Lagune jest cudownym miejscem w którym zdecydowanie warto spędzić więcej czasu.  Codziennie przypływa tam bardzo dużo turystów, od rana zajmują najlepsze miejsca wypoczynkowe. Warto wybrać się w to miejsce wcześniej, a płyną tam wodne taksówki którymi można dotrzeć rankiem i wrócić w momencie kiedy uznamy że mamy na to ochotę.</p>
+			<img src="IMAG1691_wm.jpg" width="840" height="480">
+			<p id="third">Wbrew ogólnej opinii uważam że Malta nie jest Tanim miejscem do wypoczynku. Można tam tanio dolecieć i znaleźć tani nocleg jednak ceny wyżywienia są dość wysokie. Jeśli chcemy zjeść w restauracji gdzieś w pobliżu atrakcji turystycznej to musimy się liczyć z tym, że cena dania obiadowego będzie wynosiła od 17 euro w górę. Do tego dochodzi jeszcze koszt jakiegoś picia i cena wzrasta do ponad 20 euro dla jednej osoby. Jeśli chcecie trochę zaoszczędzić warto poszukać restauracji z dala od miejsc uczęszczanych przez turystów. Mi udało się znaleźć bardzo miły lokal uczęszczany przez mieszkańców wyspy, gdzie serwowane były potrawy typowo Maltańskie a ceny były na poziomie 8 - 12 euro. Więc warto przejść się po różnych dzielnicach i sprawdzić co oferują lokalne restauracje.  
+			Na Malcie jest jeszcze bardzo dużo atrakcji które zdecydowanie trzeba odwiedzić i zobaczyć. Warto się tam wybrać przynajmniej na cały tydzień żeby docenić tą wyspę. Mam nadzieję ze te kilka wskazówek które tutaj zaprezentowałem pozwolą wam lepiej zaplanować waszą wyprawę.<p>
     </body>
 </html>
